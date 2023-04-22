1c7b075eecaa4b6a9220af7c13699b436f1a9b73
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-22 17:53:57 -0400

    routing app.js and renaming gitsection

--- new-react-portfolio/src/App.js
+++ new-react-portfolio/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+
+import Navigation from "./components/Navigation";
+import HomeSection from "./pages/Home";
+import AboutSection from "./components/aboutSection";
+import PortfolioSection from "./components/portfolioSection";
+import MusicSection from "./components/musicSection";
+import GitSection from "./components/gitSection";
+import WhoSection from "./components/WhoSection";
+import ContactSection from "./components/ContactSection";
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+
+import './reset.css';
+import './style.css';
 
 function App() {
+  const [activeSection, setActiveSection] = useState('Home');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header activeSection={activeSection} setActiveSection={setActiveSection} />
+      <main>
+        <Navigation />
+        {activeSection === 'Home' && <HomeSection />}
+        {activeSection === 'About' && <AboutSection />}
+        {activeSection === 'Portfolio' && <PortfolioSection />}
+        {activeSection === 'Music' && <MusicSection />}
+        {activeSection === 'First Git' && <FirstGitSection />}
+        {activeSection === 'Who' && <WhoSection />}
+        {activeSection === 'Contact' && <ContactSection />}
+      </main>
+      <Footer />
     </div>
   );
 }
