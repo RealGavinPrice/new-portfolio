020565926b63b1578ca74fa0f1b4c6930072c3e6
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-22 17:56:59 -0400

    writing navigation js

---
+++ new-react-portfolio/src/components/Navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <nav id="navigation">
+      <ul>
+        <li>
+          <a href="#Home">Home</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#Contact">Contact</a>
+        </li>
+        <li>
+          <a href="#Music">Music</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
