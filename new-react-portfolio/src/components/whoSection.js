3a44cc31676e27227ae0fbf33621743b96ebf163
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-27 12:03:18 -0400

    who component jsx

--- new-react-portfolio/src/components/whoSection.js
+++ new-react-portfolio/src/components/whoSection.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+function whoSection() {
+    return (
+      <section className="label" id="Who">
+        <h2 id="button">Who?</h2>
+        <br></br>
+        <h3> </h3>
+        <p></p>
+      </section>
+    );
+  }
+
+export default whoSection;
