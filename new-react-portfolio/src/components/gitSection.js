1fcfb49499848cd9c0f55e7cabdaf3c84ced8253
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-27 12:03:40 -0400

    git component jsx

--- new-react-portfolio/src/components/gitSection.js
+++ new-react-portfolio/src/components/gitSection.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+function gitSection() {
+    return (
+      <section className="label">
+        <h2 id="button">
+          <a href="https://realgavinprice.github.io/contact-moi/">First Git</a>
+        </h2>
+        <br></br>
+        <h3> </h3>
+        <p></p>
+      </section>
+    );
+  }
+
+export default gitSection;
