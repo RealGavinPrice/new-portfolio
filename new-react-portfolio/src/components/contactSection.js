2acaee5b9ac3c2758c7eb3a7958db445ae14ad4e
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-27 12:02:55 -0400

    contact jsx

--- new-react-portfolio/src/components/contactSection.js
+++ new-react-portfolio/src/components/contactSection.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+function contactSection() {
+    return (
+      <section className="label" id="Contact">
+        <h2 id="button">
+          <a className="header-link3" href="mailto:price.gavin@gmail.com">
+            Connect
+          </a>
+        </h2>
+        <br></br>
+        <h3> </h3>
+      </section>
+    );
+  }
+
+export default contactSection;
