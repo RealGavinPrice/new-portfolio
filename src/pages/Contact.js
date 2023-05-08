7ecd9e9bb203e05149ef3a8c7578aba53e77b281
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-05-08 12:36:43 -0400

    fixing hackable contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Contact() {
+const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [messageError, setMessageError] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      // Handle form submission
+    }
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+
+    if (!name) {
+      setNameError('Name is required.');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
+
+    if (!email) {
+      setEmailError('Email is required.');
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError('Email is invalid.');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (!message) {
+      setMessageError('Message is required.');
+      isValid = false;
+    } else {
+      setMessageError('');
+    }
+
+    return isValid;
+  };
+
+
+
     return (
         <div className='background-image'>
       <section className="label" id="Contact">
         <h2 id="button">
-          <a className="header-link3" href="mailto:price.gavin@gmail.com">
-            Connect
-          </a>
+        <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <div>{nameError}</div>
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <div>{emailError}</div>
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <div>{messageError}</div>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
         </h2>
         <br></br>
       </section>
       </div>
     );
   }
+
 
 export default Contact;
